9f9febd2593345a3b7c9361fca38b7e05735aaf6
lyh <631339149@qq.com>
2018-06-13 12:51:41 +0800

    First commit

---
+++ app/src/main/java/com/example/gobang/MyView.java
@@ -0,0 +1,37 @@
+package com.example.gobang;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class MyView extends View {
+    public MyView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        int widthSize=MeasureSpec.getSize(widthMeasureSpec);
+        int widthMode=MeasureSpec.getMode(widthMeasureSpec);
+        int heightSize=MeasureSpec.getSize(heightMeasureSpec);
+        int heightMode=MeasureSpec.getMode(heightMeasureSpec);
+
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+
+        super.onSizeChanged(w, h, oldw, oldh);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
