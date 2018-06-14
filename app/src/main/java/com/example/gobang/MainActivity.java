264b835015f3ed0f3abacc15dcc3b0f7e662337b
lyh <631339149@qq.com>
2018-06-14 20:15:50 +0800

    完成胜利判断和重新开始操作

--- app/src/main/java/com/example/gobang/MainActivity.java
+++ app/src/main/java/com/example/gobang/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.gobang;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private MyView myView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myView = findViewById(R.id.myView);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.restart:
+                myView.reStart();
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        动态加载menu布局。
+        getMenuInflater().inflate(R.menu.item,menu);
+        return true;
     }
 }
